981e51fe965c72667b942ec131d39472144a1ad0
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2025-04-15 00:19:27 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,24 +9,26 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Siddhartha Bhattacharjee </span>
-            from <span className="purple"> Assam, India.</span>
-            <br /> I am a third year student pursuing a Bachelor degree (BTech)
-            in Computer science engineering at SRM university Chennai.
+            from <span className="purple"> Assam, India. </span> currently based in <span className="purple"> Bangaloren </span>.
+            <br /> I’m a final-year B.Tech Computer Science Engineering student at SRM University, Chennai, with a CGPA of 9.89. I’ve gained hands-on experience through internships at <span className="purple">Broadridge</span> and <span className="purple">Siemens</span>, working on cutting-edge technologies like Blockchain, Hyperledger Fabric, Solidity, and Full-Stack Development.
             <br />
-            I am the community tech Lead at 5ireHub Chennai and the Development team head of CodeChef club SRM Rmp. I have also worked as Ambassador at Shardeum and Development team member at GDSC SRM Rmp.
+              As a passionate technologist, I’ve built impactful projects like <span className="purple">ResqAI</span> (a GenAI-powered emergency call management system) and <span className="purple">Lifafa</span> (a blockchain-based gifting platform), and I’ve <span className="purple">won 3 national-level hackathons</span> including the WarpSpeed GenAI Hackathon.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            In addition to development, I’ve <span className="purple">conducted tech workshops in 9 colleges</span> and organized events through my work with <span className="purple">Shardeum</span>, <span className="purple">Fire-Chain</span>, and college communities like <span className="purple">CodeChef SRM RMP</span> and <span className="purple">GDSC</span>. I’m always eager to learn, build, and help others grow along the way.
+              <br />
+              <br />
+              Beyond Coding, I Enjoy:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Attending conferences and Hackathons
+              <ImPointRight /> Attending tech conferences, seminars, and hackathons
             </li>
             <li className="about-activity">
-              <ImPointRight /> Teaching/ Conducting workshops
+              <ImPointRight /> Teaching and mentoring through workshops and events
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Traveling and exploring new ideas & cultures
             </li>
           </ul>
 
